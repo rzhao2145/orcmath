d792823d8cf8d789ec7b4799b2ac9a402bffa859
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-12-20 14:14:14 -0500

    commit

--- OrcMathGUI/src/guiPlayer/CatalogMaker.java
+++ OrcMathGUI/src/guiPlayer/CatalogMaker.java
@@ -6,143 +6,171 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class CatalogMaker {
 
-	private ArrayList<Countries> list;
-	
+	public static Scanner in;
+
+	private ArrayList<Book> catalog;
+
 	public CatalogMaker() {
-		list = new ArrayList<Countries>();
-		list.add(new Countries("United States", "Washington D.C", "January"));
-		list.add(new Countries("Canada", "Ottawa", "July"));
-		list.add(new Countries("Sweden", "Stockholm", "March"));
-		list.add(new Countries("France", "Paris", "December"));
-		list.add(new Countries("Mexico", "Mexico City", "June"));
+		//instantiate the catalog
+		catalog = new ArrayList<Book>();
 	}
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		CatalogMaker cm = new CatalogMaker();
-//		System.out.println(cm.getCSVContent());
-		
-//		System.out.println("Type a country");
-//		String cty = in.nextLine();
-//		System.out.println("Type the capital");
-//		String cap = in.nextLine();
-//		System.out.println("Type the month you visited the place");
-//		String month = in.nextLine();
-//		cm.addNewItem(cty,cap,month);
-		
-		System.out.println("What do you want to name to file?");
-		cm.testSaveContent(in.nextLine());
-		
+	public static void main(String[] args){
+		CatalogMaker maker = new CatalogMaker();
+		in = new Scanner(System.in);
+		maker.menu();
 	}
-	
-	
-	
-	public String getCSVContent() {
-		String data = "Country,Capital,Month Visited \n";
-		for(Countries c : list) {
-			data += c + "\n";
+
+	private static void displayMessage(String message){
+		System.out.println(message);
+	}
+
+	private void menu() {
+		displayMessage("Would you like to \"load\" a save file or \"create\" a new list? ");
+		String[] allowedEntry = {"load","create"};
+		String input = getEntry(allowedEntry);
+		if(input.equals("load")){
+			load();
+		}else{
+			create();
 		}
-		return data;
 	}
-	
-	public void addNewItem(String country, String capital, String monthVisited) {
-		list.add(new Countries(country,capital,monthVisited));
-		System.out.println("Country added successfully!");
-	}
-	
-	private void testSaveContent(String fileName) {
 
-		try {
-
-			FileWriter fw = new FileWriter(fileName);
-
-			for(Countries c : list) {
-				fw.write(c + "\n");
+	private void create() {
+		
+		boolean running = true;
+		while(running){
+			showCatalog();
+			displayMessage("Would you like to \"add\", \"save\", or \"quit\"?");
+			String[] allowedEntry = {"add","save","quit"};
+			String selection = getEntry(allowedEntry);
+			if(selection.equals("add")){
+				add();
+			}else if(selection.equals("save")){
+				save();
+			}else{
+				running = false;
 			}
-			
-			fw.write("This file was created programmatically.");
-
-			fw.close();
-
-			System.out.println("Success! File \"" + fileName + "\" saved!");
-
-		} catch (IOException e) {
-
-			System.out.println(
-					"An IOException was thrown. \nCheck to see that the directory where you tried to save the file actually exists.");
-
 		}
-
 	}
-	
-	
-	private  List<String> testFileLoading() {
 
-		Scanner in = new Scanner(System.in);
+	private void add() {
+		String title = null;
+		String author = null;
+		int pages = 0;
+		displayMessage("Please enter a title");
+		title = getStringInput();
+		displayMessage("Please enter an author");
+		author = getStringInput();
+		displayMessage("Please enter the number of pages.");
+		pages = getIntegerInput();
+		addBook(new Book(title, author, pages));
+	}
 
+	private int getIntegerInput() {
+		String text = in.nextLine();
+		int value = 0;
+		boolean valid = false;
+		while(!valid){
+			try{
+				value = Integer.parseInt(text);
+				valid = true;
+			}catch(NumberFormatException nfe){
+				displayMessage("You must enter an integer.");
+			}
+		}
+		return value;
+	}
+
+	private static String getStringInput(){
+		String text = in.nextLine();
+		while(text.isEmpty()){
+			displayMessage("You must enter a non-empty String.");
+			text = in.nextLine();
+		}
+		return text;
+	}
+
+
+	private void addBook(Book b){
+		catalog.add(b);
+	}
+
+	private void save() {
+		try{    
+			FileWriter fw=new FileWriter("BookCatalog.csv");
+			for(Book b: catalog){
+				fw.write(b+"\n");    	
+			}
+
+			fw.close();    
+			System.out.println("Success! File \"BookCatalog.csv\" saved!");
+		}catch(IOException e){
+			System.out.println("An IOException was thrown. \nCheck to see that the directory where you tried to save the file actually exists.");
+		}
+	}
+
+	private static String getEntry(String[] allowedEntry) {
+		String input = in.nextLine();
+		while(!matchesEntry(input, allowedEntry)){
+			displayMessage("You must enter one of these words: ");
+			for(String s: allowedEntry){
+				System.out.print(s+" ");
+			}
+			displayMessage("\n");
+			input = in.nextLine();
+		}
+		return input;
+	}
+
+	private static boolean matchesEntry(String text, String[] list){
+		for(String l: list){
+			if(l.equals(text))return true;
+		}
+		return false;
+	}
+
+	private  void showCatalog() {
+		displayMessage("The catalog contains these Books:\n");
+		for(Book b: catalog){
+			displayMessage("   "+b.toString()+"\n");
+		}
+	}
+
+	private void load() {
 		String fileName = "";
-
-		List<String> content = new ArrayList<String>();
-
-		// use this boolean to control the while loop. The user should have multiple
-		// chances to enter a correct filename
-
+		//empty the catalog to prepare for a new load
+		catalog = new ArrayList<Book>();
+		//use this boolean to control the while loop. The user should have multiple chances to enter a correct filename
 		boolean opened = false;
-
-		while (!opened) {
-
+		while(!opened){
 			try {
-
 				System.out.println("Enter a file to open");
-
 				fileName = in.nextLine();
-
 				FileReader fileReader = new FileReader(new File(fileName));
-
 				String line = "";
-
-				// a BufferedReader enables us to read teh file one line at a time
-
+				//a BufferedReader enables us to read teh file one line at a time
 				BufferedReader br = new BufferedReader(fileReader);
-
 				while ((line = br.readLine()) != null) {
+
 					String[] param = line.split(",");
-					content.add(param[0]);
-					/*
-					 * 
-					 * useful later:
-					 * 
-					 * split only a comma that has an even number of quotes ahead of it
-					 * 
-					 * String[] row = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-					 * 
-					 */
+					//add a new Book for each line in the save file
+					catalog.add(new Book(param[0],param[1],Integer.parseInt(param[2])));
+
+
 
 				}
-
 				br.close();
-
 				opened = true;
-
-			} catch (IOException e) {
-
+			}catch (IOException e) {
 				System.out.println("The file name you specified does not exist.");
-
 			}
-
 		}
-
-		// close the Scanner
-
-		in.close();
-
-		return content;
+		create();
 
 	}
-	
 }
