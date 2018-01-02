ea66752eae975c24d3d392230eae73ea7cfba507
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2018-01-02 15:28:18 -0500

    Custom Button 1/2/18

---
+++ OrcMathGUI/src/myStuff/CustomButton.java
@@ -0,0 +1,49 @@
+package myStuff;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import guiPlayer.CustomLabel;
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+
+public class CustomButton extends Button implements CustomLabel{
+
+	private String string1;
+	private String string2;
+	
+
+	public CustomButton(int x, int y, int w, int h, String text, Action action) {
+		super(x, y, w, h, text, action);
+		string1 = "String 1";
+		string2 = "String 2";
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void updateString1(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateString2(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setIconColor(Color color) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void drawButton(Graphics2D g, boolean hover) {
+		g.drawString(string1, 200, 200);
+		g.drawString(string2, 200, 300);
+		g.setColor(Color.BLACK);
+		
+	}
+
+}
--- OrcMathGUI/src/myStuff/Raymond.java
+++ OrcMathGUI/src/myStuff/Raymond.java
@@ -1,5 +1,27 @@
 package myStuff;
 
-public class Raymond {
+
+import guiTeacher.GUIApplication;
+
+public class Raymond extends GUIApplication{
+	
+	private static Raymond ramond;
+	private static CustomButton customButton;
+
+	public Raymond(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Raymond raymond = new Raymond(1000,1000);
+	}
+
+	@Override
+	public void initScreen() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
